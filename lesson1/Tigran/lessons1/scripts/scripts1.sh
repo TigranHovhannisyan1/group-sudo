2a8ba09adf0ef832640ecae94f5354bca6cf9fac
Tigran <armenia159753@gmail.com>
2018-03-05 21:12:10 +0400

    Added lesson1

---
+++ lesson1/Tigran/lessons1/scripts/scripts1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /workspace/lessons/lessons1/output
+rm -rf U1
+mkdir U1
+cd U1
+mkdir U2 U3
+touch a.txt
+echo "Karen" >a.txt
+cd U2
+touch a.txt b.txt
+echo "Nvard">a.txt
+echo " Tigran">b.txt
+
---
+++ lesson1/Tigran/lessons1/scripts/scripts2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /workspace/lessons/lessons1/output/U1
+echo " U1:a.txt:"; cat a.txt
+cd /workspace/lessons/lessons1/output/U1/U2
+$A cat a.txt
+echo "U2:a.txt:" .$A 
+echo "U2:b.txt"; cat  b.txt

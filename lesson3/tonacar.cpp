ef7d0ef8ab11e3ecfdaa638acb73968cd8bf2fe5
Tigran <armenia159753@gmail.com>
2018-04-04 02:18:47 +0400

    added Homeworks

---
+++ lesson3/tonacar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    int m = 11, k = 11;
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j < 22; j++) {
+            if (j >= m && j <= k) {
+                std::cout << '*';
+            } else {
+                std::cout << ' ';
+            }
+        }
+        m--;
+        k++;
+        std::cout <<'\n';
+    }
+    return 0;
+}
---
+++ lesson3/zuygTver.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main() {
+    int first, last;
+    std::cout << "Input first number : ";
+    std::cin >> first;
+    std::cout << "Input last number : ";
+    std::cin >> last;
+    if(first < last) {
+    for (int i = first; i <= last; i++) {
+        if (i % 2 == 0) {
+            std::cout << i << '\n';
+            }
+        }
+    } else {
+        std::cout << "Error!!";
+    }
+    return 0;
+}

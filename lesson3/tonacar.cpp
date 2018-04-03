0c7f226fe05b331a32e81da5673e63b696fbedd2
Tigran <armenia159753@gmail.com>
2018-04-04 02:25:24 +0400

    Added Homeworks

--- lesson3/tonacar.cpp
+++ lesson3/tonacar.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
-    int m = 11, k = 11;
+    int m = 111111111111111111111111111111, k = 11;
     for (int i = 0; i < 11; i++) {
         for (int j = 0; j < 22; j++) {
             if (j >= m && j <= k) {
--- lesson3/zuygTver.cpp
+++ lesson3/zuygTver.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 int main() {
     int first, last;
-    std::cout << "Input first number : ";
+    std::cout << " Input first number : ";
     std::cin >> first;
-    std::cout << "Input last number : ";
+    std::cout << " Input last number : ";
     std::cin >> last;
     if(first < last) {
-    for (int i = first; i <= last; i++) {
-        if (i % 2 == 0) {
-            std::cout << i << '\n';
+        for (int i = first; i <= last; i++) {
+            if (i % 2 == 0) {
+                std::cout << i << '\n';
             }
         }
     } else {

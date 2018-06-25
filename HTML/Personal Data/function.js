c41141698dfd2aeead6f5f067a9ff4c96069a164
TigranHovhannisyan1 <armenia159753@gmail.com>
2018-06-25 18:20:23 +0400

    Personal Data Modified

--- HTML/Personal Data/function.js
+++ HTML/Personal Data/function.js
@@ -7,20 +7,34 @@ function to_realize (fornm1) {
     }
     var str = [ gender, fornm1 ['Name'].value,fornm1 ['Lastname'].value ].join('  ');
     document.getElementById ('tool').innerHTML = str;
-    document.getElementById ('modal').style.display = 'block';
+    // document.getElementById ('modal').style.display = 'block';
     document.getElementById ('form1').style.display = 'none';
+    document.getElementById ('container-form1').style.display = 'none';
+    fromyou = document.getElementById ('fromyou');
+    fromyou.style.display = 'block';
 }
-    document.getElementsByClassName("continue")[0].onclick = function() {
-        document.getElementById ('modal').style.color.red;
-        fromyou = document.getElementById ('fromyou');
-        fromyou.style.display = 'block';
-}
+
 function info_cart(form2) {
     var table ="<table ><tr><td> Name: </td> <td>";
-    table += form2["name"].value + "</td></tr><tr><td>Surtname: </td> <td>"+ form2["surname"].value + "</td></tr><tr><td>Adress: </td> <td>"+ form2["adress"].value + "</td></tr><tr><td>City: </td> <td>"+ form2["city"].value + "</td></tr><tr><td>Country: </td> <td>"+ form2["country"].value + "</td></tr><tr><td>Phone: </td> <td>"+ form2["phone"].value + "</td></tr> </table>";
+    table += form2["name"].value + "</td></tr><tr><td>Surtname: </td> <td>"+ 
+    form2["surname"].value + "</td></tr><tr><td>Adress: </td> <td>"+ form2["adress"].value + 
+    "</td></tr><tr><td>City: </td> <td>"+ form2["city"].value + "</td></tr><tr><td>Country: </td> <td>"+ 
+    form2["country"].value + "</td></tr><tr><td>Phone: </td> <td>"+ form2["phone"].value + "</td></tr> </table>";
     document.getElementById("info-container").innerHTML += table;
     for (var i = 0; i < 6; i++) {
         form2[i].value = "";  
     };
+}
+
+function saveLS() {
+    var divs = localStorage.getItem('divs') || '[]';
+    divs.push({"name": form2["name"].value, "Surname": form2["surname"].value});
+    localStorage.setItem('divs', JSON.stringify(divs));
+    divs = JSON.parse(divs);
+    for (var i = 0; i < divs.length; i++) {
+        var hert = divs[i];
+        var el = '<table>' + hert.name+''
+        console.log(divs[i]);
+    }
     
 }
